896583b09eec319033bd0e6375a8c5b219db2b4a
Fyanli <yanlifu@email.com>
2021-10-26 20:59:07 +0200

    submit of sql oflab

---
+++ sqlintro.sql
@@ -0,0 +1,10 @@
+use sakila;
+select * from film;
+select title from film;
+select name as "language" from language;
+select count(store_id) from store;
+select count(*) from staff;
+select first_name from staff;
+
+
+
